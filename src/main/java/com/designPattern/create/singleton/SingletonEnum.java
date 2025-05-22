5ec83ab3dc8a147a23284f11ac5ac38642ed281f
dog <liqianli@anshare.cc>
2025-05-22 11:50:09 +0800

    add:单例模式enum构建v6

--- src/main/java/com/designPattern/create/singleton/SingletonEnum.java
+++ src/main/java/com/designPattern/create/singleton/SingletonEnum.java
@@ -33,6 +33,7 @@ public enum SingletonEnum {
         }
         System.out.println("alen....");
         System.out.println("deal....");
+        System.out.println("bella...");
         return 0;
     }
 
